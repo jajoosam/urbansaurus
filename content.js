3dff0deab67bc87c835cf3a76aa1ddfeca0591f5
jajoosam <jajoosam@github.com>
2017-08-07 09:33:32 +0530

    Better CSS

--- content.js
+++ content.js
@@ -5,6 +5,10 @@ function getWord(name,url) {
     return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
+function titleCase(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 var url = window.location.href;
 var word = getWord('q',url);
 console.log(word);
@@ -27,9 +31,9 @@ $.getJSON(searchTerm, function(data) {
     console.log(result.reference);
     result.link = data.list[0].permalink;
     console.log(result.link);
-
-    output = '<div class="cardc3"> <div class="containerc3"><h3 class="card-header">Urban word: ' + result.word + '</h5> <p class="card-body"><b>Defination</b></br>' + result.defination + '</br><b>Example</b></br>' + result.example + '</br><b>Referenced with</b></br>' + result.reference + '</p> <div class="card-footer center text-center"><p><a href="' + result.link + '">More results</a></p></div> </div></div>';
-    $(output).insertAfter("#extabar");
+    output = '<div class="demo-card-event mdl-card mdl-shadow--2dp"> <div class="mdl-card__title mdl-card--expand"> <h4 class="card-header"><i>Urban</i></br></br><b>' + titleCase(result.word) + '</b></h4> <p class="card-body"><b>Defination</b></br>' + result.defination + '</br><b>Example</b></br>' + result.example + '</br><b>Referenced with</b></br>' + result.reference + '</p> </div> <div class="mdl-card__actions mdl-card--border"> <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="'+ result.link + '">More results</a> </div>';
+    //output = '<div class="cardc3"> <div class="containerc3"><h3 class="card-header">Urban word: ' + result.word + '</h5> <p class="card-body"><b>Defination</b></br>' + result.defination + '</br><b>Example</b></br>' + result.example + '</br><b>Referenced with</b></br>' + result.reference + '</p> <div class="card-footer center text-center"><p><a href="' + result.link + '">More results</a></p></div> </div></div>';
+    $(output).insertBefore("#taw");
     
 });
 
